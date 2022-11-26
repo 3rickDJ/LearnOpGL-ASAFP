a210117667dffa666469497855e3c33325f2759a
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-26 09:10:58 -0600

    update: class Obstacle

--- include/Obstacle.h
+++ include/Obstacle.h
@@ -4,7 +4,13 @@ class Obstacle {
 public:
     Obstacle();
     float x,y,z;
+    float tx, ty, tz;
+    float radius;
     float dx,dy,dz;
     float rotation;
     float speedRotation;
+
+    void loadVars(float x, float y, float z, float dx, float dy, float dz, float rotation, float speedRotation,
+                  float radius);
+    void loadVars(float x, float y, float z, float radius);
 };
--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -7,6 +7,36 @@ Obstacle::Obstacle() {
     this->dx = 0;
     this->dy = 0;
     this->dz = 0;
-    this->speedRotation =0;
+    this->radius = 0;
+    this->tx = 0;
+    this->ty = 0;
+    this->tz = 0;
     this->rotation = 0;
+    this->speedRotation = 0;
+}
+
+void Obstacle::loadVars(float x, float y, float z, float dx, float dy, float dz, float rotation, float speedRotation,
+                        float radius) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->dx = dx;
+    this->dy = dy;
+    this->dz = dz;
+    this->radius = radius;
+    this->tx = x;
+    this->ty = y;
+    this->tz = z;
+    this->rotation = rotation;
+    this->speedRotation = speedRotation;
+}
+
+void Obstacle::loadVars(float x, float y, float z, float radius) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->radius = radius;
+    this->tx = x;
+    this->ty = y;
+    this->tz = z;
 }
