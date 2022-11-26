263540d4cdf3868871dd988ad1578171d6b82724
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-26 07:51:03 -0600

    add: class Obstacle

---
+++ include/Obstacle.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Obstacle {
+public:
+    Obstacle();
+    float x,y,z;
+    float dx,dy,dz;
+    float rotation;
+    float speedRotation;
+};
---
+++ src/Obstacle.cpp
@@ -0,0 +1,12 @@
+#include "Obstacle.h"
+
+Obstacle::Obstacle() {
+    this->x = 0;
+    this->y = 0;
+    this->z = 0;
+    this->dx = 0;
+    this->dy = 0;
+    this->dz = 0;
+    this->speedRotation =0;
+    this->rotation = 0;
+}
