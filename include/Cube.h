e065090377e40903b2e86c3b504628996f872fdb
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-27 15:14:34 -0600

    Src de javi

--- include/Cube.h
+++ include/Cube.h
@@ -3,6 +3,8 @@
 #include "Textures.h"
 
 class Cube : public Obstacle {
+    void drawFace(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float x4,
+                  float y4, float z4);
 
 public:
     Cube();
@@ -12,7 +14,4 @@ public:
     void addTexture(char *name, int face);
 
     void animation();
-
-    void drawFace(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float x4,
-                  float y4, float z4);
 };
